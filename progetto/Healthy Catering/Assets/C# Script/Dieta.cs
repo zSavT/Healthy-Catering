45041b1724d0cce2ff7805fb70bcf0a8bde6d9c4
zSavT <verioc3@gmail.com>
2022-03-30 22:14:08 +0200

    Aggiunti costruttori e distruttori per le classi Cliente - Dieta - Ristorante

--- progetto/Healthy Catering/Assets/C# Script/Dieta.cs
+++ progetto/Healthy Catering/Assets/C# Script/Dieta.cs
@@ -2,4 +2,14 @@ public class Dieta
 {
     string nome = "";
     string descrizione = "";
+
+    public Dieta(string nome, string descrizione)
+    {
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+    ~Dieta()
+    {
+        
+    }
 }
