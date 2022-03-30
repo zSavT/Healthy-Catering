4eb93e853aab6b2f56a6bfceda8dceda25fd7215
zSavT <verioc3@gmail.com>
2022-03-30 22:20:15 +0200

    Aggiunti costruttori e distruttori per le classi Piatto e Player

--- progetto/Healthy Catering/Assets/C# Script/Piatto.cs
+++ progetto/Healthy Catering/Assets/C# Script/Piatto.cs
@@ -9,6 +9,23 @@ public class Piatto
 
     List <OggettoQuantita <Ingrediente>> ingredienti = null;
 
+    public Piatto(string nome, string descrizione, float costo, float costoEco, int nutriScore, List<OggettoQuantita<Ingrediente>> ingredienti)
+    {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.costo = costo;
+        this.costoEco = costoEco;
+        this.nutriScore = nutriScore;
+        this.ingredienti = ingredienti;
+    }
+
+    ~Piatto()
+    {
+        
+    }
+
     //TODO metodo che trova patologie compatibili in base agli ingredienti
     //TODO metodo che trova diete compatibili in base agli ingredienti
+
+
 }
