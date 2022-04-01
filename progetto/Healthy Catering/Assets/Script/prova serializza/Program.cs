7158ec105f04d9e4c4f90050d5289fb642a525be
AlessandroCarella <alessandro.carella.universita@gmail.com>
2022-04-01 17:05:11 +0200

    aggiornata la classe Serializza per salvare e leggere liste di oggetti

--- progetto/Healthy Catering/Assets/Script/prova serializza/Program.cs
+++ progetto/Healthy Catering/Assets/Script/prova serializza/Program.cs
@@ -1,79 +1,77 @@
-﻿using System.Globalization;
-using System.Data.Common;
-using System.Xml;
-using System.Reflection;
-using System;
-using System.IO;
+﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Script;
-
 public class Serializza
 {
-    static string pathJson = Directory.GetCurrentDirectory();
-    
-    public static void Main()
+    public static void salvaOggettiSuFile <Oggetto> (List <Oggetto> oggetti)
     {
-        Serializza.salvaOggettoSuFile(new Dieta());
-    }
-
-    public Serializza (string className){
-        pathJson = pathJson + @"\" + className + ".json";
-    }
-
-    public void salvaOggettoSuFile <Oggetto> (Oggetto oggetto)
-    {
-        string oggettoSerializzato = JsonConvert.SerializeObject(oggetto, Newtonsoft.Json.Formatting.Indented);
-        File.WriteAllText(@pathJson, oggettoSerializzato);
-
-        /*
-        Esempio di utilizzo:
-            Serializza tempSerializza = new Serializza ("Cliente");
-            
-            Cliente tempCliente = new Cliente
+        if (oggetti.Count > 0){
+            string pathJson = Directory.GetCurrentDirectory() + @"\" + oggetti[0].GetType().Name + ".json";
+            using (StreamWriter file = File.CreateText(@pathJson))
             {
-                nome = "Bad Boys",
-                dieta = 1996
-            };
-
-            tempSerializza.salvaOggettoSuFile <Cliente> (tempCliente);        
-        */
-    }
-
-    public Oggetto leggiOggettoDaFile <Oggetto> (string filePath)
-    {
-        using (StreamReader file = File.OpenText(@filePath))
-        using (JsonTextReader reader = new JsonTextReader(file))
-        {
-            JObject oggettoLetto = (JObject) JToken.ReadFrom(reader); //oggetto di tipo JObject
-            return oggettoLetto.ToObject <Oggetto>();
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented;
+                serializer.Serialize(file, oggetti);
+            }
+        }
+        else{
+            throw new InvalidOperationException("Lista passata vuota, progressi non salvati (?)");
         }
 
         /*
         Esempio di utilizzo:
-            string path = @"C:\Users\alex1\Desktop\Healthy-Catering\progetto\Healthy Catering\Assets\C# Script\prova serializza\Cliente.json";
-            
-            Serializza tempSerializza = new Serializza ("Cliente");
-            
-            Cliente tempCliente = tempSerializza.leggiOggettoDaFile <Cliente> (path);
+            List <Dieta> diete = new List<Dieta> ();
+            diete.Add (new Dieta ("1"));
+            diete.Add (new Dieta ("2"));
+            try{
+                Serializza.salvaOggettiSuFile<Dieta> (diete);
+            }
+            catch (InvalidOperationException e){
+                Console.WriteLine (e.Message);
+            }
+        */
+    }
 
-            Console.WriteLine (tempCliente.ToString ());
+    public static List<Oggetto> leggiOggettiDaFile <Oggetto> (string filePath)
+    {
+        if (File.Exists(filePath)){
+            string json = File.ReadAllText(@filePath);
+            return JsonConvert.DeserializeObject<List<Oggetto>>(json);
+        }
+        else{
+            throw new FileNotFoundException ("File non trovato, salva un salvataggio di una lista della classe che mi stai passando per leggerla!");
+        }
+
+        /*
+        Esempio di utilizzo:
+            try{
+                List <Dieta> idk = Serializza.leggiOggettiDaFile<Dieta> (@"C:\Users\alex1\Desktop\Healthy-Catering\progetto\Healthy Catering\Assets\Script\prova serializza\Dieta.json");
+                foreach (var dieta in idk){
+                    Console.WriteLine (dieta);
+                }
+            }
+            catch (FileNotFoundException e){
+                Console.WriteLine (e.Message);
+            }
         */
     }
 }
 
 /*
 La classe Cliente utilizzata negli esempi:
-    public class Cliente
+    public class Dieta
     {
         public string nome = "";
-        
-        public int dieta = 0;
+
+        public Dieta(string nome)
+        {
+            this.nome = nome;
+        }
 
         public override string ToString()
         {
-            return nome + "\n" + dieta;
+            return nome;
         }
     }
 */
