272dc627d7c8b98deb057445721794d6da7c73f1
zSavT <verioc3@gmail.com>
2022-04-03 20:35:57 +0200

    Reso lo script modulare e generico

--- src/ScriptUnity/Interactor.cs
+++ src/ScriptUnity/Interactor.cs
@@ -5,7 +5,8 @@ using UnityEngine.Events;
 public class Interactor : MonoBehaviour
 {
     [SerializeField] private LayerMask layerUnity = 6;              //layer utilizzato da Unity per le categorie di oggetto
-    private UnityEvent onInteract;                                  //attributo per l'invocazione di eventi all'interno di Unity            
+    private UnityEvent onInteract;                                  //attributo per l'invocazione di eventi all'interno di Unity    
+    [SerializeField] private KeyCode tastoInterazione;              //tasto da premere per invocare l'azione
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +22,11 @@ public class Interactor : MonoBehaviour
             if(colpito.collider.GetComponent<Interactable>() != false)
             {
                 onInteract = colpito.collider.GetComponent<Interactable>().onInteract;
-                if(Input.GetKeyDown(KeyCode.E))
+                if(Input.GetKeyDown(tastoInterazione))
                 {
                     /*
                         Quello che c'è qui dentro viene eseguito quando il player inquadra l'oggetto e preme il comando.
-                        Debug.Log(colpito.collider.name);
+                        Debug.Log(colpito.collider.name);       //per test in game
                     */
                     onInteract.Invoke();
                 }
