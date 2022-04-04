b45dab761625ba3abf8851c643fd32902241c04d
zSavT <f.cassano45@studenti.uniba.it>
2022-04-04 14:48:59 +0200

    Rinominato file

--- src/ScriptUnity/CameraGiocatore.cs
+++ src/ScriptUnity/CameraGiocatore.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraGiocatore : MonoBehaviour
 {
-    [SerializeField]public float sensX;                     //sensibilit√† mouse asse x
-    [SerializeField]public float sensY;                     //sensibilit√† mouse asse y
+    [SerializeField] public float sensX;                     //sensibilit‡ mouse asse x
+    [SerializeField] public float sensY;                     //sensibilit‡ mouse asse y
 
-    public Transfrom orientamento;                          //per l'orientazione del player in game
+    public Transform orientamento;                          //per l'orientazione del player in game
 
     float xRotation;
     float yRotation;
 
-    private void Start() 
+    private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -27,11 +27,12 @@ public class CameraGiocatore : MonoBehaviour
 
         xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                          //permette di muovere la visuale solo di 90 grandi in su e in gi√π
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                          //permette di muovere la visuale solo di 90 grandi in su e in gi˘
 
         //movimento camera
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientamento.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
+
 
