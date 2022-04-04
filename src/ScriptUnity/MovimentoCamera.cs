9548c8a7c406045e97a923351e6c0cb9e0168798
zSavT <55914219+zSavT@users.noreply.github.com>
2022-04-04 23:28:27 +0200

    Update MovimentoCamera.cs

--- src/ScriptUnity/MovimentoCamera.cs
+++ src/ScriptUnity/MovimentoCamera.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-    Lo script va inserito nell'oggetto vuoto che contiene l'effettiva camera del gioco
-*/
+ *   Lo script va inserito nell'oggetto vuoto che contiene l'effettiva camera del gioco
+ */
 public class MovimentoCamera : MonoBehaviour
 {
     public Transform PosizioneCamera;           //va collegata in Unity all'oggetto posizione camera su Unity
