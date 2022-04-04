4916a59659f49afa7ad2fe61aa36512a5199058c
zSavT <55914219+zSavT@users.noreply.github.com>
2022-04-04 23:29:45 +0200

    Update Animazione.cs

--- src/ScriptUnity/Animazione.cs
+++ src/ScriptUnity/Animazione.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-*   Script da inserire sul modello 3D
-*/
+ *   Script da inserire sul modello 3D
+ */
 
 
 public class Animazione : MonoBehaviour
