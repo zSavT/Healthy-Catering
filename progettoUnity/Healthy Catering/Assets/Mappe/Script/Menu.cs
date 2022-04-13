1b84bfd6dd7f43e0570f841624f1fe0c099fd829
zSavT <f.cassano45@studenti.uniba.it>
2022-04-13 18:50:47 +0200

    Aggiunto menu Opzioni

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/Menu.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/Menu.cs
@@ -7,11 +7,21 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void chiudi()
     {
         Application.Quit();
+    }
+
+    public void menuOpzioni()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void menuPrincipale()
+    {
+        SceneManager.LoadScene(0);
     }
 }
