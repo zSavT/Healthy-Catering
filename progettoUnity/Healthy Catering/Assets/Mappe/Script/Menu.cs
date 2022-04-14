88653eead92a1ebbb87f83d4429da4d4cf71233f
zSavT <verioc3@gmail.com>
2022-04-14 13:09:27 +0200

    Aggiunta bozza caricamento e scomparsa bottoni

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/Menu.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/Menu.cs
@@ -1,13 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void PlayGame()
+
+    public UnityEvent allAvvio;             //serve per eliminare altri elementi in visualilzzazione
+
+
+
+
+    public void PlayGame(int sceneIndex)
     {
-        SceneManager.LoadScene(2);
+        allAvvio.Invoke();
+        StartCoroutine(CaricamentoAsincrono(sceneIndex));
+        allAvvio.Invoke();
+    }
+
+    IEnumerator CaricamentoAsincrono(int sceneIndex)
+    {
+        AsyncOperation caricamento = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!caricamento.isDone)
+        {
+            float progresso = Mathf.Clamp01(caricamento.progress / .9f);
+            Debug.Log(caricamento.progress);
+            yield return null;
+        }
     }
 
     public void chiudi()
