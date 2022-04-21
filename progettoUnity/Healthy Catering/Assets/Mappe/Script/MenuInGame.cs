82f5af5ef786220ba917649d67f4ea43bfda4a00
zSavT <f.cassano45@studenti.uniba.it>
2022-04-21 17:25:56 +0200

    Aggiunto menu in game

---
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/MenuInGame.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInGame : MonoBehaviour
+{
+    [Header("Menu Opzioni")]
+    public KeyCode tastoMenu;
+
+    private bool giocoInPausa = false;
+    [SerializeField] public GameObject menuPausa;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        giocoInPausa = false;
+        menuPausa.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkTastoMenu();
+    }
+
+    private bool checkTastoMenu()
+    {
+        if(Input.GetKeyDown(tastoMenu))
+        {
+            if(giocoInPausa)
+            {
+                resumeGame();
+            } else
+            {
+                pauseGame();
+            }
+        }
+        return true;
+    }
+
+
+    void resumeGame()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale = 1f; //sblocca il tempo
+        giocoInPausa = false;
+    }
+
+
+    void pauseGame()
+    {
+        menuPausa.SetActive(true);
+        Time.timeScale = 0f; //blocca il tempo
+        giocoInPausa = true;
+    }
+}
