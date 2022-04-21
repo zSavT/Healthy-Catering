7c7779ffc3d66ba5f290ca2a23636dfcae2b2aba
zSavT <f.cassano45@studenti.uniba.it>
2022-04-21 17:58:59 +0200

    Fix bug tasto indietro menu opzioni

---
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/menuOpzioni.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuOpzioni : MonoBehaviour
+{
+    public void menuPrincipale()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
