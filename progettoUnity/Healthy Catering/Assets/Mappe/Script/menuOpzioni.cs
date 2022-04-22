7fdc3e1a98bdced396af0a0b9f09a99995d3395e
zSavT <verioc3@gmail.com>
2022-04-22 12:16:27 +0200

    Terminate impostazioni Volume - Grafica -- Schermo Intero - Risoluzione

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/menuOpzioni.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/menuOpzioni.cs
@@ -2,15 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class MenuOpzioni : MonoBehaviour
 {
-   
+
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown risoluzioniDisponibili;
+
+    public Resolution[] risoluzioni;
+
+    void Start()
+    {
+        risoluzioni = Screen.resolutions;
+        risoluzioniDisponibili.ClearOptions();      //svuota le scelte
+        List<string> opzioni = new List<string>();
+        int indiceRisoluzioneCorrente = 0;
+        for (int i = 0; i < risoluzioni.Length; i++)
+        {
+            string risoluzione = risoluzioni[i].width + " x " + risoluzioni[i].height;
+            opzioni.Add(risoluzione);
+            if (risoluzioni[i].width == Screen.currentResolution.width &&
+                risoluzioni[i].height == Screen.currentResolution.height)
+            {
+                indiceRisoluzioneCorrente = i;
+            }
+        }
+        risoluzioniDisponibili.AddOptions(opzioni);
+        risoluzioniDisponibili.value = indiceRisoluzioneCorrente; ;
+        risoluzioniDisponibili.RefreshShownValue();
+    }
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setQualita(int indiceQualita)
+    {
+        QualitySettings.SetQualityLevel(indiceQualita);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 
     public void menuPrincipale()
     {
         SceneManager.LoadScene(0);
     }
 
+     public void setRisoluzione(int risoluzioneSelezionata)
+    {
+        Resolution risoluzione = risoluzioni[risoluzioneSelezionata];
+        Screen.SetResolution(risoluzione.width, risoluzione.height, Screen.fullScreen);
+    }
 }
