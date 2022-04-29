b1a2f43139881596d89f7e2bff97656523b285c3
zSavT <verioc3@gmail.com>
2022-04-29 13:02:36 +0200

    Aggiunto percorso NPC per camminata

---
+++ progettoUnity/Healthy Catering/Assets/AINPC.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AINPC : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        updateDestinazione();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            iterazioneIndex();
+            updateDestinazione();
+        }
+    }
+
+    void updateDestinazione()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+    }
+
+    void iterazioneIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
