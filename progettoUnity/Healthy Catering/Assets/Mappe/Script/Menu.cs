3ccf2162bfc59faf1ce5f001cdc8bc8373f5dd45
zSavT <verioc3@gmail.com>
2022-05-10 19:09:28 +0200

    Aggiunta selezione livelli

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/Menu.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/Menu.cs
@@ -7,8 +7,6 @@ using Wilberforce;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private Slider sliderCaricamento;        //slider del caricamento della partita
-    [SerializeField] private UnityEvent allAvvio;             //serve per eliminare altri elementi in visualilzzazione
     [SerializeField] private UnityEvent clickCrediti;             //serve per eliminare altri elementi in visualilzzazione
     [SerializeField] private Camera camera;
 
@@ -25,28 +23,14 @@ public class Menu : MonoBehaviour
     }
 
 
-    public void playGame(int sceneIndex)
-    {
-        allAvvio.Invoke();
-        StartCoroutine(caricamentoAsincrono(sceneIndex));
-        allAvvio.Invoke();
-    }
-
-    IEnumerator caricamentoAsincrono(int sceneIndex)
-    {
-        AsyncOperation caricamento = SceneManager.LoadSceneAsync(sceneIndex);
-
-        while (!caricamento.isDone)
-        {
-            float progresso = Mathf.Clamp01(caricamento.progress / .9f);
-            sliderCaricamento.value = progresso;
-            yield return null;
-        }
-    }
-
     public void crediti()
     {
         clickCrediti.Invoke();
+    }
+
+    public void menuSelezioneLivelli()
+    {
+        SceneManager.LoadScene(3);
     }
 
     public void menuOpzioni()
---
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/SelezioneLivelli.cs
@@ -0,0 +1,69 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SelezioneLivelli : MonoBehaviour
+{
+    [SerializeField] private Button bottoneLivello0;
+    [SerializeField] private Button bottoneLivello1;
+    [SerializeField] private Button bottoneLivello2;
+    [SerializeField] private Slider sliderCaricamento;        //slider del caricamento della partita
+    [SerializeField] private UnityEvent allAvvio;             //serve per eliminare altri elementi in visualilzzazione
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  PlayerPrefs.SetInt("livello1", 0);
+       // PlayerPrefs.SetInt("livello2", 0);
+        if (PlayerPrefs.GetInt("livello1") == 0)
+        {
+            var colors = bottoneLivello1.colors;
+            colors.normalColor = Color.grey;
+            colors.highlightedColor = Color.grey;
+            colors.pressedColor = Color.grey;
+            colors.selectedColor = Color.grey;
+            bottoneLivello1.colors = colors;
+        }
+        if (PlayerPrefs.GetInt("livello2") == 0)
+        {
+            var colors = bottoneLivello2.colors;
+            colors.normalColor = Color.grey;
+            colors.highlightedColor = Color.grey;
+            colors.pressedColor = Color.grey;
+            colors.selectedColor = Color.grey;
+            bottoneLivello2.colors = colors;
+        }
+    }
+
+    public void menuPrincipale()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void caricaLivello0()
+    {
+
+    }
+
+    public void playGame(int sceneIndex)
+    {
+        allAvvio.Invoke();
+        StartCoroutine(caricamentoAsincrono(sceneIndex));
+    }
+
+    IEnumerator caricamentoAsincrono(int sceneIndex)
+    {
+        AsyncOperation caricamento = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!caricamento.isDone)
+        {
+            float progresso = Mathf.Clamp01(caricamento.progress / .9f);
+            sliderCaricamento.value = progresso;
+            yield return null;
+        }
+    }
+}
