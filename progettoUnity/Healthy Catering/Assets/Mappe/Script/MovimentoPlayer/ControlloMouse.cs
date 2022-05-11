56662febd893506e26a2a84810be8faa88ffa574
zSavT <verioc3@gmail.com>
2022-05-11 14:03:09 +0200

    Aggiunti fix richiesti

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/MovimentoPlayer/ControlloMouse.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/MovimentoPlayer/ControlloMouse.cs
@@ -16,6 +16,7 @@ public class ControlloMouse : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sensibilitaMouse = 250f;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         this.puoCambiareVisuale = true;
