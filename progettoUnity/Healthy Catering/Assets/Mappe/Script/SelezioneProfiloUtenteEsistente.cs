8e72f0e1e70ec2c073eee9e6a9c6f72e4a45ddf3
zSavT <verioc3@gmail.com>
2022-05-15 22:14:08 +0200

    Terminato creazione/modifica/selezione profilo giocatore

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/SelezioneProfiloUtenteEsistente.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/SelezioneProfiloUtenteEsistente.cs
@@ -7,11 +7,18 @@ using UnityEngine;
 public class SelezioneProfiloUtenteEsistente : MonoBehaviour
 {
 
-
+    [SerializeField] private GameObject elementiGenereNeutro;
     [SerializeField] private TMP_Dropdown dropDownListaPlayer;
+    [SerializeField] private TMP_Dropdown dropDownGenere;
+    [SerializeField] private TMP_Dropdown dropDownColorePelle;
+    [SerializeField] private TMP_Dropdown dropDownModello3D;
     private List<Player> player;
     private List<string> nomiPlayerPresenti;
     private string nomeSelezionato = "";
+    private int sceltaGenere;
+    private int sceltaColorePelle;
+    private int sceltaModelloPlayer;
+    bool genereNeutroScelto = false;
 
 
     // Start is called before the first frame update
@@ -22,18 +29,102 @@ public class SelezioneProfiloUtenteEsistente : MonoBehaviour
         // letturaNomiUtenti();
         aggiuntaNomiDropdown();
         nomeSelezionato = dropDownListaPlayer.options[dropDownListaPlayer.value].text;
+        dropDownGenere.value = caricaGenereGiocatore(nomeSelezionato);
+        dropDownColorePelle.value = caricaColorePelle(nomeSelezionato);
+        dropDownModello3D.value = caricaGenereModello(nomeSelezionato);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void refreshValori()
     {
-
+        dropDownGenere.value = caricaGenereGiocatore(nomeSelezionato);
+        dropDownColorePelle.value = caricaColorePelle(nomeSelezionato);
+        dropDownModello3D.value = caricaGenereModello(nomeSelezionato);
     }
 
     public void salvaImpostazioni()
     {
         salvaNomePlayerGiocante(nomeSelezionato);
+        salvaGenereGiocatore(nomeSelezionato, sceltaGenere);
+        salvaColorePelle(nomeSelezionato, sceltaColorePelle);
+        if (genereNeutroScelto)
+        {
+            salvaGenereModello(nomeSelezionato, sceltaModelloPlayer);
+        }
         SceneManager.LoadScene(0);
+    }
+
+    public void setSceltaModelloGiocatore(int indice)
+    {
+        sceltaModelloPlayer = indice;
+    }
+
+    public void setGenerePlayer(int indiceScelta)
+    {
+        sceltaGenere = indiceScelta;
+        if (indiceScelta == 2)
+        {
+            genereNeutroScelto = true;
+            elementiGenereNeutro.SetActive(true);
+        }
+        else
+        {
+            genereNeutroScelto = false;
+            elementiGenereNeutro.SetActive(false);
+        }
+    }
+
+    public void setPellePlayer(int indice)
+    {
+        sceltaColorePelle = indice;
+    }
+
+
+    public void salvaGenereModello(string nomeGiocatore, int scelta)
+    {
+
+        PlayerPrefs.SetInt(nomeGiocatore + "_modello", scelta);
+
+    }
+
+    public void salvaNomePlayerGiocante(string nomeInserito)
+    {
+
+        PlayerPrefs.SetString("PlayerName", nomeInserito);
+
+    }
+
+    public void salvaColorePelle(string nomeGiocatore, int scelta)
+    {
+
+        PlayerPrefs.SetInt(nomeGiocatore + "_pelle", scelta);
+
+    }
+
+    public void salvaGenereGiocatore(string nomeGiocatore, int scelta)
+    {
+        PlayerPrefs.SetInt(nomeGiocatore + "_genere", scelta);
+        if (scelta == 0 || scelta == 1)
+        {
+            salvaGenereModello(nomeGiocatore, scelta);
+        }
+    }
+
+
+    public int caricaGenereModello(string nomeGiocatore)
+    {
+        return PlayerPrefs.GetInt(nomeGiocatore + "_modello");
+    }
+
+
+    public int caricaColorePelle(string nomeGiocatore)
+    {
+        return PlayerPrefs.GetInt(nomeGiocatore + "_pelle");
+    }
+
+
+    public int caricaGenereGiocatore(string nomeGiocatore)
+    {
+        return PlayerPrefs.GetInt(nomeGiocatore + "_genere");
     }
 
     public void indiceSceltaNomeUtente(int indice)
@@ -75,13 +166,9 @@ public class SelezioneProfiloUtenteEsistente : MonoBehaviour
         */
         nomiPlayerPresenti.Add("sav");
         nomiPlayerPresenti.Add("pippo");
+        nomiPlayerPresenti.Add("nicola");
         dropDownListaPlayer.AddOptions(nomiPlayerPresenti);
     }
 
-    public void salvaNomePlayerGiocante(string nomeInserito)
-    {
 
-        PlayerPrefs.SetString("PlayerName", nomeInserito);
-
-    }
 }
