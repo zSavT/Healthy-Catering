8487b0eb261cc1e6d2dc3909856ba7e7b65c3153
AlessandroCarella <alessandro.carella.universita@gmail.com>
2022-06-01 22:11:58 +0200

    fix di tutto

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/PannelloMagazzino.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/PannelloMagazzino.cs
@@ -1,9 +1,3 @@
-/*
-    
- 
- 
- */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,16 +10,20 @@ public class PannelloMagazzino : MonoBehaviour
     [SerializeField] private Image sfondoImmaginePC;
 
     //mi serve per settare il parent dell'oggetto sotto a questo oggetto, poi se la vede unity a sistemarli all'interno della schermata
-    [SerializeField] private GameObject PannelloMostraInventario;
-    [SerializeField] private GameObject sottoPannelloMostraInventarioXElementi;
-    private int bottoniMassimiPerPannelloMostraInventarioXElementi = 5;
-    private int numeroPannelliMostraInventarioXElementiPresenti = 1;
-    private Button mostraSingoloIngredienteTemplate;
+    [SerializeField] private GameObject pannelloMostraInventario;
+    [SerializeField] private GameObject pannelloXElementi;
+    private int bottoniMassimiPerPannelloXElementi = 4;
+    private int numeroPannelliXElementiPresenti = 1;
+    private Button bottoneIngredienteTemplate;
 
     private void Start()
     {
         pannelloMagazzino.SetActive(false);
-        mostraSingoloIngredienteTemplate = sottoPannelloMostraInventarioXElementi.GetComponentInChildren<Button>();
+        //creo una copia del bottone template
+        bottoneIngredienteTemplate = Instantiate(pannelloXElementi.GetComponentInChildren<Button>());
+        //poi lo elimino dal pannello cosi che non ci sia piu' (non posso eliminare l'instanza successivamente siccome ci sono piu' pannelli 
+        //al posto di uno solo come in menu (interazione cliente))
+        pannelloXElementi = rimuoviTuttiFigliDaPannello(pannelloXElementi);
     }
 
     public void attivaPannello()
@@ -37,51 +35,58 @@ public class PannelloMagazzino : MonoBehaviour
     private void popolaSchermata()
     {
         List<Ingrediente> databaseIngredienti = Database.getDatabaseOggetto(new Ingrediente());
-        Player giocatore = Database.getPlayerDaNome (PlayerSettings.caricaNomePlayerGiocante ());
+        List <OggettoQuantita <int>> inventario = Database.getPlayerDaNome (PlayerSettings.caricaNomePlayerGiocante ()).inventario;
         
-
         int bottoniAggiuntiFinoAdOra = 0;
 
-        foreach (OggettoQuantita <int> oggettoDellInventario in giocatore.inventario)
+        foreach (OggettoQuantita <int> oggettoDellInventario in inventario)
         {
-            Button bottoneDaAggiungereTemp = creaBottoneConValoriSingoloIngrediente(oggettoDellInventario, mostraSingoloIngredienteTemplate, databaseIngredienti);
-            
-            bottoneDaAggiungereTemp.transform.SetParent(sottoPannelloMostraInventarioXElementi.transform, false);
+            Button bottoneDaAggiungereTemp = creaBottoneConValoriIngrediente(oggettoDellInventario, bottoneIngredienteTemplate, databaseIngredienti);
+            bottoneDaAggiungereTemp.transform.SetParent(pannelloXElementi.transform, false);
             bottoniAggiuntiFinoAdOra++;
 
-            if (bottoniAggiuntiFinoAdOra > bottoniMassimiPerPannelloMostraInventarioXElementi)
+            if (bottoniAggiuntiFinoAdOra > bottoniMassimiPerPannelloXElementi)
             {
-                aggiungiPannelloMostraInventarioXElementi();
+                aggiungiPannelloXElementi();
                 bottoniAggiuntiFinoAdOra = 0;
             }
         }
-
-        Destroy(mostraSingoloIngredienteTemplate);
     }
 
-    private Button creaBottoneConValoriSingoloIngrediente(OggettoQuantita<int> oggettoDellInventario, Button mostraSingoloIngredienteTemplate, List <Ingrediente> databaseIngredienti)
+    private Button creaBottoneConValoriIngrediente(OggettoQuantita<int> oggettoDellInventario, Button bottoneIngredienteTemplate, List <Ingrediente> databaseIngredienti)
     {
-        Ingrediente temp = Ingrediente.idToIngrediente(oggettoDellInventario.oggetto, databaseIngredienti);
+        Ingrediente ingrediente = Ingrediente.idToIngrediente(oggettoDellInventario.oggetto, databaseIngredienti);
      
-        Button output = Instantiate(mostraSingoloIngredienteTemplate);
+        Button output = Instantiate(bottoneIngredienteTemplate);
+        output.name = ingrediente.nome;
 
-        output.name = temp.nome;
-
-        output.GetComponentsInChildren<TextMeshProUGUI>()[0].text = temp.nome;
+        output.GetComponentsInChildren<TextMeshProUGUI>()[0].text = ingrediente.nome;
         output.GetComponentsInChildren<TextMeshProUGUI>()[1].text = oggettoDellInventario.quantita.ToString ();
 
         return output;
     }
     
-    private void aggiungiPannelloMostraInventarioXElementi()
+    private void aggiungiPannelloXElementi()
     {
-        GameObject nuovoSottoPannelloMostraInventarioXElementi = Instantiate (sottoPannelloMostraInventarioXElementi);
-        numeroPannelliMostraInventarioXElementiPresenti++;
-        nuovoSottoPannelloMostraInventarioXElementi.name = "SottoPannelloMostraInventarioXElementi" + numeroPannelliMostraInventarioXElementiPresenti.ToString ();
+        GameObject nuovoPannelloXElementi = Instantiate (pannelloXElementi);
+        nuovoPannelloXElementi = rimuoviTuttiFigliDaPannello(nuovoPannelloXElementi);
 
-        nuovoSottoPannelloMostraInventarioXElementi.transform.SetParent(PannelloMostraInventario.transform, false);
+        nuovoPannelloXElementi.name = "SottoPannelloMostraInventarioXElementi" + numeroPannelliXElementiPresenti.ToString ();
+        nuovoPannelloXElementi.transform.SetParent(pannelloMostraInventario.transform, false);
+        numeroPannelliXElementiPresenti++;
+
         //ora la variabile di prima viene usata per il nuovo pannello
-        sottoPannelloMostraInventarioXElementi = nuovoSottoPannelloMostraInventarioXElementi;
+        pannelloXElementi = nuovoPannelloXElementi;
+    }
+
+    private GameObject rimuoviTuttiFigliDaPannello(GameObject pannello)
+    {
+        foreach (Transform child in pannello.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        return pannello;//non sono sicuro sia necessario il return del pannello, se non serve poi lo togliamo
     }
 
     public void cambiaSfondo()
