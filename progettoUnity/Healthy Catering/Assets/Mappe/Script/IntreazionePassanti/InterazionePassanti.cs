942e5d47848d458631ba61196705e6b9a7ed96c5
AlessandroCarella <alessandro.carella.universita@gmail.com>
2022-06-04 20:08:03 +0200

    iniziata implementazione interazione passanti

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/IntreazionePassanti/InterazionePassanti.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/IntreazionePassanti/InterazionePassanti.cs
@@ -1,11 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InterazionePassanti : MonoBehaviour
 {
+    [SerializeField] private LayerMask layerUnityNPCPassivi = 7;
+    [SerializeField] private GameObject pannelloInterazionePassanti;
+    [SerializeField] private TextMeshProUGUI testoInterazionePassanti;
+
+    [SerializeField] private UnityEvent playerStop;
+    [SerializeField] private UnityEvent playerRiprendiMovimento;
+
     private void Start()
     {
-        
+        pannelloInterazionePassanti.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (NPCPassivoPuntato())
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                apriPannelloInterazionePassanti();
+            }
+        }
+    }
+
+    private bool NPCPassivoPuntato()
+    {
+        RaycastHit NPCPassivoInquadrato;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out NPCPassivoInquadrato, 3, layerUnityNPCPassivi))
+        {
+            return true;
+        }
+        print("ciao2");
+        return false;
+    }
+
+    private void apriPannelloInterazionePassanti()
+    { 
+        pannelloInterazionePassanti.SetActive(true);
+        testoInterazionePassanti.text = trovaScrittaDaMostrare();
+
+    }
+
+    private string trovaScrittaDaMostrare()
+    {
+        return "ciao";
     }
 }
