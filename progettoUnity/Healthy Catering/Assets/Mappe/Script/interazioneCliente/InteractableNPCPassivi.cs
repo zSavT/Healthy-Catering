35ef6d46b2cf82748b98ffdf592e11932385f052
AlessandroCarella <alessandro.carella.universita@gmail.com>
2022-06-07 18:50:52 +0200

    aggiunto lookAt

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/InteractableNPCPassivi.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/InteractableNPCPassivi.cs
@@ -29,9 +29,12 @@ public class InteractableNPCPassivi : MonoBehaviour
     {
         //Il movimento è istantaneo per renderlo più smooth si può usare eventualmente la stessa logica CountText() in HUB
         //in alternativa si puoò usare il GameObject.FindGameObjectWithTag("Player").transform;
+        /*
         Transform npcPos = gameObject.transform.parent;         //parent perchè la rotazione è dovuta dal contenitore in game e non dal modello
         Vector3 delta = new Vector3(posizionePlayer.rotation.x - npcPos.position.x, 0.0f, posizionePlayer.rotation.z - npcPos.position.z);
         Quaternion rotation = Quaternion.LookRotation(delta);
         gameObject.transform.parent.localRotation = rotation;
+        */
+        gameObject.transform.parent.LookAt(posizionePlayer);
     }
 }
