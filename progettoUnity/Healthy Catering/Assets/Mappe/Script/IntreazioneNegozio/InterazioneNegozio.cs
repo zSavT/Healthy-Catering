4999fad40867410c33e3e63d54746dc50aae3cef
Alessandro Carella <79840231+AlessandroCarella@users.noreply.github.com>
2022-06-09 18:49:00 +0200

    aggiornato prefab pannello menu

---
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/IntreazioneNegozio/InterazioneNegozio.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class InterazioneNegozio
+{
+
+}
