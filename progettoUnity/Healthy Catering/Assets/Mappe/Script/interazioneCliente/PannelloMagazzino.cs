3d45accdbfb753c72dd3396c97bcca36786b9fb4
zSavT <verioc3@gmail.com>
2022-06-16 23:01:16 +0200

    Fix

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/PannelloMagazzino.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/PannelloMagazzino.cs
@@ -33,7 +33,6 @@ public class PannelloMagazzino : MonoBehaviour
         //poi lo elimino dal pannello cosi che non ci sia piu' (non posso eliminare l'instanza successivamente siccome ci sono piu' pannelli 
         //al posto di uno solo come in menu (interazione cliente))
         pannelloXElementi = rimuoviTuttiFigliDaPannello(pannelloXElementi);
-
         schermataMagazzinoPopolata = false;
     }
 
@@ -43,7 +42,7 @@ public class PannelloMagazzino : MonoBehaviour
         pannelloMagazzino.SetActive(true);
         pannelloMagazzinoAperto = true;
         pannelloMostraRicette.chiudiPannelloMostraRicette();
-        
+        cambiaSfondoDesktop();
         if (!schermataMagazzinoPopolata)
             popolaSchermata();
     }
@@ -53,6 +52,7 @@ public class PannelloMagazzino : MonoBehaviour
         pannelloMagazzino.SetActive(false);
         pannelloMagazzinoAperto = false;
         pannelloMostraRicette.chiudiPannelloMostraRicette();
+
     }
 
     public bool getPannelloMagazzinoAperto()
@@ -62,23 +62,31 @@ public class PannelloMagazzino : MonoBehaviour
 
     public void cambiaSfondoMagazzino()
     {
-        sfondoImmaginePC.sprite = Resources.Load<Sprite>("SchermataMagazzino");
+        if (AspectRatio(Screen.height, Screen.width) == "4:3" || AspectRatio(Screen.height, Screen.width) == "3:4")
+            sfondoImmaginePC.sprite = Resources.Load<Sprite>("SchermataMagazzino4_3");
+        else
+            sfondoImmaginePC.sprite = Resources.Load<Sprite>("SchermataMagazzino");
+
+
     }
 
     public void cambiaSfondoDesktop()
     {
-        sfondoImmaginePC.sprite = Resources.Load<Sprite>("SfondoBasePC");
+        if (AspectRatio(Screen.height, Screen.width) == "4:3" || AspectRatio(Screen.height, Screen.width) == "3:4")
+            sfondoImmaginePC.sprite = Resources.Load<Sprite>("SfondoBasePC4_3");
+        else
+            sfondoImmaginePC.sprite = Resources.Load<Sprite>("SfondoBasePC");
     }
 
     private void popolaSchermata()
     {
         List<Ingrediente> databaseIngredienti = Database.getDatabaseOggetto(new Ingrediente());
         List<Piatto> databasePiatti = Database.getDatabaseOggetto(new Piatto()); //mi serve per vedere le ricette
-        List <OggettoQuantita <int>> inventario = Database.getPlayerDaNome (PlayerSettings.caricaNomePlayerGiocante ()).inventario;
-        
+        List<OggettoQuantita<int>> inventario = Database.getPlayerDaNome(PlayerSettings.caricaNomePlayerGiocante()).inventario;
+
         int bottoniAggiuntiFinoAdOra = 0;
 
-        foreach (OggettoQuantita <int> oggettoDellInventario in inventario)
+        foreach (OggettoQuantita<int> oggettoDellInventario in inventario)
         {
             Button bottoneDaAggiungereTemp = creaBottoneConValoriIngrediente(oggettoDellInventario, bottoneIngredienteTemplate, databaseIngredienti, databasePiatti);
             bottoneDaAggiungereTemp.transform.SetParent(pannelloXElementi.transform, false);
@@ -87,7 +95,7 @@ public class PannelloMagazzino : MonoBehaviour
             if (bottoniAggiuntiFinoAdOra > bottoniMassimiPerPannelloXElementi)
             {
                 if (oggettoDellInventario != inventario[inventario.Count - 1]) // se e' diverso dall'ultimo elemento, previene che venga creato un pannello vuoto
-                { 
+                {
                     aggiungiPannelloXElementi();
                     bottoniAggiuntiFinoAdOra = 0;
                 }
@@ -97,30 +105,30 @@ public class PannelloMagazzino : MonoBehaviour
         schermataMagazzinoPopolata = true;
     }
 
-    private Button creaBottoneConValoriIngrediente(OggettoQuantita<int> oggettoDellInventario, Button bottoneIngredienteTemplate, List <Ingrediente> databaseIngredienti, List<Piatto> databasePiatti)
+    private Button creaBottoneConValoriIngrediente(OggettoQuantita<int> oggettoDellInventario, Button bottoneIngredienteTemplate, List<Ingrediente> databaseIngredienti, List<Piatto> databasePiatti)
     {
         Ingrediente ingrediente = Ingrediente.idToIngrediente(oggettoDellInventario.oggetto, databaseIngredienti);
-     
+
         Button output = Instantiate(bottoneIngredienteTemplate);
         output.name = ingrediente.nome;
 
         output.GetComponentsInChildren<TextMeshProUGUI>()[0].text = "Ingrediente: " + ingrediente.nome;
-        output.GetComponentsInChildren<TextMeshProUGUI>()[1].text = "Quantità presente: " + oggettoDellInventario.quantita.ToString ();
-       
+        output.GetComponentsInChildren<TextMeshProUGUI>()[1].text = "Quantità presente: " + oggettoDellInventario.quantita.ToString();
+
         output.GetComponentsInChildren<Button>()[1].onClick.AddListener(() => {
             pannelloMostraRicette.apriPannelloMostraRicette(ingrediente, databaseIngredienti, databasePiatti);
             pannelloInventarioCanvas.SetActive(false);
-        }); 
+        });
 
         return output;
     }
-    
+
     private void aggiungiPannelloXElementi()
     {
-        GameObject nuovoPannelloXElementi = Instantiate (pannelloXElementi);
+        GameObject nuovoPannelloXElementi = Instantiate(pannelloXElementi);
         nuovoPannelloXElementi = rimuoviTuttiFigliDaPannello(nuovoPannelloXElementi);
 
-        nuovoPannelloXElementi.name = "SottoPannelloMostraInventarioXElementi" + numeroPannelliXElementiPresenti.ToString ();
+        nuovoPannelloXElementi.name = "SottoPannelloMostraInventarioXElementi" + numeroPannelliXElementiPresenti.ToString();
         nuovoPannelloXElementi.transform.SetParent(pannelloMostraInventario.transform, false);
         numeroPannelliXElementiPresenti++;
 
@@ -136,6 +144,26 @@ public class PannelloMagazzino : MonoBehaviour
         }
 
         return pannello;//non sono sicuro sia necessario il return del pannello, se non serve poi lo togliamo
+    }
+
+    /// <summary>
+    /// Metodo che permette di calcolare l'aspect ratio del gioco.
+    /// </summary>
+    /// <param name="a">Altezza risoluzione</param>
+    /// <param name="b">Larghezza risoluzione</param>
+    /// <returns>Rapporto Altezza/risoluzione</returns>
+    private string AspectRatio(int a, int b)
+    {
+        int r;
+        int oa = a;
+        int ob = b;
+        while (b != 0)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return (oa / a).ToString() + ":" + (ob / a).ToString();
     }
 
 }
