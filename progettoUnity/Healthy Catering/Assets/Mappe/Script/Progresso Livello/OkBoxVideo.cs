ff6ec4267edcb3ee6dda36696b84ecb8503a8419
Alessandro Carella <79840231+AlessandroCarella@users.noreply.github.com>
2022-06-26 19:26:58 +0200

    prima implementazione okBoxVideo

---
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/Progresso Livello/OkBoxVideo.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class OkBoxVideo : MonoBehaviour
+{
+    [SerializeField] private GameObject pannello;
+    [SerializeField] private TextMeshProUGUI titolo;
+    [SerializeField] private TextMeshProUGUI testo;
+    [SerializeField] private Image immagineOGIF;
+    [SerializeField] private Button bottoneConferma;
+
+    public static readonly int meccanicheServire = 0;
+    public static readonly int finitiIngredienti = 1;
+    public static readonly int doveEIlNegozio = 2;
+    public static readonly int interazioneNPC = 3;
+
+    List<string> titoli = new List<string>
+    {
+        "spiegazione meccaniche per servire",
+        "sono finiti gli ingredienti per fare i piatti",
+        "Dov'e' il negozio",
+        "interazione con gli npc"
+    };
+
+    List<string> testi = new List<string>
+    {
+        "ciao testo",
+        "idk testo"
+    };
+
+    void Start()
+    {
+        pannello.SetActive(false);
+        titolo.text = "";
+        testo.text = "";
+        immagineOGIF = null;
+    }
+
+    public void apriOkBoxVideo(int posizione)
+    {
+        if (posizione < titoli.Count)
+        {
+            titolo.text = titoli[posizione];
+            testo.text = testi[posizione];
+        }
+        else
+        {
+            titolo.text = "errore";
+            testo.text = "";
+        }
+        cambiaImmagine(posizione);
+    }
+
+    public void chiudiOkBoxVideo()
+    {
+        pannello.SetActive(false);
+        titolo.text = "";
+        testo.text = "";
+        immagineOGIF = null;
+    }
+
+    private void cambiaImmagine(int posizione)
+    {
+        Sprite nuovaImmagine = Resources.Load<Sprite>("immaginiAiuto/" + "immagineOGifOkBoxVideo" + titoli[posizione]);
+        if (nuovaImmagine == null)
+        {
+            nuovaImmagine = null;
+        }
+        immagineOGIF.sprite = nuovaImmagine;
+    }
+
+    
+}
