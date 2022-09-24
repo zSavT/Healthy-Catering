268143e732b3a26a3027968b3948a993c6871f9d
Alessandro Carella <79840231+AlessandroCarella@users.noreply.github.com>
2022-09-24 18:48:39 +0200

    refactoring interactor

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/Interactor.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/interazioneCliente/Interactor.cs
@@ -50,9 +50,6 @@ public class Interactor : MonoBehaviour
     [SerializeField] private UnityEvent inquadratoNPC;
     [SerializeField] private UnityEvent uscitaRangeMenu;
 
-
-    private Vector3 posizioneCameraOriginale;
-
     public static bool menuApribile;                                                      //se il menu opzioni è aperto, le interezioni sono disattivate
     public static bool pannelloAperto;
     private int IDClientePuntato;
@@ -92,7 +89,6 @@ public class Interactor : MonoBehaviour
         }
         chiudiPannello();
         pannelloAperto = false;
-        posizioneCameraOriginale = mainCamera.transform.position;
         menuApribile = true;
         progresso.setGiocatore(giocatore);
     }
@@ -112,152 +108,275 @@ public class Interactor : MonoBehaviour
         menuApribile = false;
     }
 
+    private void interazioneUtenteConNPCVari()
+    {
+        if (menuApribile)
+        {
+            gestisciOggettiPuntatiEMenuAribiliConTasto();
+
+            gestioneChiusureMenu();
+        }
+    }
+    private void gestisciOggettiPuntatiEMenuAribiliConTasto()
+    {
+        if (NPCClientePuntato())
+        {
+            gestisciNPCpuntato();
+        }
+        else if (pcPuntato())
+        {
+            gestisciPCpuntato();
+        }
+        else if (NPCPassantePuntato())
+        {
+            gestisciNPCpassantePuntato();
+        }
+        else if (negoziantePuntato())
+        {
+            gestiscinegoziantePuntato();
+        }
+        else if (portaPuntata())
+        {
+            gestisciPortaPuntata();
+        }
+        else if (Input.GetKeyDown(testoRicettario) && nuovaSchermataApribile())
+        {
+            gestisciAperturaRicettario();
+        }
+        else if (Input.GetKeyDown(tastoMenuAiuto) && nuovaSchermataApribile())
+        {
+            gestisciAperturaMenuAiuto();
+        }
+        else
+        {
+            negozio.animazioneNPCIdle();
+            uscitaRangeMenu.Invoke();
+            if (pannelloAperto)
+            {
+                if (!PannelloMenu.pannelloIngredientiPiattoAperto && !PannelloMenu.pannelloConfermaPiattoAperto && !PannelloMenu.pannelloIngredientiGiustiSbagliatiAperto)
+                {
+                    if (Input.GetKeyDown(KeyCode.Escape))
+                    {
+                        esciDaInterazioneCliente();
+                    }
+                }
+            }
+        }
+    }
+
+    private bool NPCClientePuntato()
+    {
+        RaycastHit NPCpuntato;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out NPCpuntato, 2, layerUnityNPC))
+        {
+            // se l'oggetto visualizzato è interagibile
+            if (NPCpuntato.collider.GetComponent<Interactable>() != false)
+            {
+                IDClientePuntato = NPCpuntato.collider.GetComponent<Interactable>().IDCliente;
+                npc = NPCpuntato.collider.GetComponent<Interactable>();
+                ; return true;
+            }
+        }
+        return false;
+    }
+
+    private void gestisciNPCpuntato()
+    {
+        if (npc.raggiuntoBancone)
+        {
+            inquadratoNPC.Invoke();
+            if (Input.GetKeyDown(tastoInterazione))
+            {
+                interazioneCliente(IDClientePuntato);
+            }
+        }
+        else
+        {
+            uscitaRangeMenu.Invoke();
+        }
+    }
+
+    private void interazioneCliente(int IDCliente)
+    {
+        muoviCameraPerInterazioneCliente();
+
+        playerStop.Invoke();
+
+        pannelloMenuCliente.setCliente(IDClientePuntato, giocatore, npc);
+
+        PuntatoreMouse.abilitaCursore();
+        pannelloApertoChiuso();
+    }
+
+    private void muoviCameraPerInterazioneCliente()
+    {
+        mainCamera.transform.position = posizioneCameraMenuCliente.transform.position;
+    }
+
+    private void pannelloApertoChiuso()
+    {
+        pannelloAperto = !pannelloAperto;
+    }
+
+    private void gestisciPCpuntato()
+    {
+        inquadratoNPC.Invoke();
+        if (Input.GetKeyDown(tastoInterazione) && !magazzino.getPannelloMagazzinoAperto())
+        {
+            magazzino.apriPannelloMagazzino(giocatore);
+            playerStop.Invoke();
+            PuntatoreMouse.abilitaCursore();
+            CambioCursore.cambioCursoreNormale();
+        }
+    }
+
+    private void gestisciNPCpassantePuntato()
+    {
+        inquadratoNPC.Invoke();
+        if (Input.GetKeyDown(tastoInterazione) && !(interazionePassanti.getPannelloInterazionePassantiAperto()))
+        {
+            interazionePassanti.apriPannelloInterazionePassanti(npcPassivo.transform.parent.name);
+            npcPassivo.animazioneParlata(gameObject.transform);
+            playerStop.Invoke();
+            PuntatoreMouse.abilitaCursore();
+            CambioCursore.cambioCursoreNormale();
+        }
+    }
+
+    private void gestiscinegoziantePuntato()
+    {
+        inquadratoNPC.Invoke();
+        negozio.animazioneNPCInquadrato();
+        if (Input.GetKeyDown(tastoInterazione) && !(negozio.getPannelloAperto()))
+        {
+            negozio.apriPannelloNegozio(giocatore);
+            playerStop.Invoke();
+            PuntatoreMouse.abilitaCursore();
+            CambioCursore.cambioCursoreNormale();
+        }
+    }
+
+    private void gestisciPortaPuntata()
+    {
+        inquadratoNPC.Invoke();
+        if (Input.GetKeyDown(tastoInterazione) && !(negozio.getPannelloAperto()))
+        {
+            suonoNegozio.Play();
+            this.gameObject.transform.position = destinazioneTeleport.transform.position;
+            nelRistorante = !nelRistorante;
+            Debug.Log(nelRistorante);
+        }
+    }
+    
+    private bool nuovaSchermataApribile()
+    {
+        return (
+            !negozio.getPannelloAperto()
+            &&
+            !pannelloMenuCliente.getPannelloMenuClienteAperto()
+            &&
+            !ricettarioScript.getRicettarioAperto()
+            &&
+            !menuAiuto.getPannelloMenuAiutoAperto()
+        );
+    }
+
+    private void gestisciAperturaRicettario()
+    {
+        playerStop.Invoke();
+        ricettarioScript.apriRicettario();
+        PuntatoreMouse.abilitaCursore();
+        CambioCursore.cambioCursoreNormale();
+    }
+
+    private void gestisciAperturaMenuAiuto()
+    {
+        playerStop.Invoke();
+        menuAiuto.apriPannelloMenuAiuto();
+        PuntatoreMouse.abilitaCursore();
+        CambioCursore.cambioCursoreNormale();
+    }
+
+    private void gestioneChiusureMenu()
+    {
+        if (negozio.getPannelloAperto() && !negozio.getPannelloConfermaAperto())
+        {
+            gestisciChiusuraNegozio();
+        }
+        if (interazionePassanti.getPannelloInterazionePassantiAperto())
+        {
+            gestisciChiusuraPannelloPassanti();
+        }
+        if (ricettarioScript.getRicettarioAperto())
+        {
+            gestisciChiusuraRicettario();
+        }
+        if (menuAiuto.getPannelloMenuAiutoAperto())
+        {
+            gestisciChiusuraMenuAiuto();
+        }
+    }
+
+    private void gestisciChiusuraNegozio()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            negozio.chiudiPannelloNegozio();
+            PuntatoreMouse.disabilitaCursore();
+            playerRiprendiMovimento.Invoke();
+        }
+    }
+
+    private void gestisciChiusuraPannelloPassanti()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            interazionePassanti.chiudiPannelloInterazionePassanti();
+            npcPassivo.stopAnimazioneParlata(gameObject.transform);
+            PuntatoreMouse.disabilitaCursore();
+            playerRiprendiMovimento.Invoke();
+        }
+    }
+
+    private void gestisciChiusuraRicettario()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ricettarioScript.chiudiRicettario();
+            PuntatoreMouse.disabilitaCursore();
+            playerRiprendiMovimento.Invoke();
+        }
+    }
+
+    
+            
+
+private void gestisciChiusuraMenuAiuto()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuAiuto.chiudiPannelloMenuAiuto();
+            PuntatoreMouse.disabilitaCursore();
+            playerRiprendiMovimento.Invoke();
+        }
+    }
+
     private bool NPCPassantePuntato()
     {
         RaycastHit NPCPassivoInquadrato;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out NPCPassivoInquadrato, 3, layerUnityNPCPassivi))
         {
-            if(NPCPassivoInquadrato.collider.GetComponent<InteractableNPCPassivi>() != false)
-            { 
+            if (NPCPassivoInquadrato.collider.GetComponent<InteractableNPCPassivi>() != false)
+            {
                 npcPassivo = NPCPassivoInquadrato.collider.GetComponent<InteractableNPCPassivi>();
                 return true;
             }
-            
+
         }
         return false;
     }
 
-    private void interazioneUtenteConNPCVari()
-    {
-        if (menuApribile)
-        {
-            if (NPCClientePuntato())
-            {
-                if(npc.raggiuntoBancone)
-                {
-                    inquadratoNPC.Invoke();
-                    if (Input.GetKeyDown(tastoInterazione))
-                    {
-                        interazioneCliente(IDClientePuntato);
-                    }
-                } else
-                {
-                    uscitaRangeMenu.Invoke();
-                }
-            } else if (pcPuntato())
-            {
-                inquadratoNPC.Invoke();
-                if (Input.GetKeyDown(tastoInterazione) && !magazzino.getPannelloMagazzinoAperto())
-                {
-                    magazzino.apriPannelloMagazzino(giocatore);
-                    playerStop.Invoke();
-                    PuntatoreMouse.abilitaCursore();
-                    CambioCursore.cambioCursoreNormale();
-                }
-            }
-            else if (NPCPassantePuntato())
-            {
-                inquadratoNPC.Invoke();
-                if (Input.GetKeyDown(tastoInterazione) && !(interazionePassanti.getPannelloInterazionePassantiAperto()))
-                {
-                    interazionePassanti.apriPannelloInterazionePassanti(npcPassivo.transform.parent.name);
-                    npcPassivo.animazioneParlata(gameObject.transform);
-                    playerStop.Invoke();
-                    PuntatoreMouse.abilitaCursore();
-                    CambioCursore.cambioCursoreNormale();
-                }
-            } else if (negozianteInquadrato())
-            {
-                inquadratoNPC.Invoke();
-                negozio.animazioneNPCInquadrato();
-                if (Input.GetKeyDown(tastoInterazione) && !(negozio.getPannelloAperto()))
-                {
-                    negozio.apriPannelloNegozio(giocatore);
-                    playerStop.Invoke();
-                    PuntatoreMouse.abilitaCursore();
-                    CambioCursore.cambioCursoreNormale();
-                }
-            }
-            else if (portaInquadrata())
-            {
-                inquadratoNPC.Invoke();
-                if (Input.GetKeyDown(tastoInterazione) && !(negozio.getPannelloAperto()))
-                {
-                    suonoNegozio.Play();
-                    this.gameObject.transform.position = destinazioneTeleport.transform.position;
-                    nelRistorante = !nelRistorante;
-                    Debug.Log(nelRistorante);
-                }
-            } else if (Input.GetKeyDown(testoRicettario) && !(negozio.getPannelloAperto()) && !pannelloMenuCliente.getPannelloMenuClienteAperto() && !ricettarioScript.getRicettarioAperto() && !menuAiuto.getPannelloMenuAiutoAperto())
-            {
-                playerStop.Invoke();
-                ricettarioScript.apriRicettario();
-                PuntatoreMouse.abilitaCursore();
-                CambioCursore.cambioCursoreNormale();
-            } else if (Input.GetKeyDown(tastoMenuAiuto) && !(negozio.getPannelloAperto()) && !pannelloMenuCliente.getPannelloMenuClienteAperto() && !ricettarioScript.getRicettarioAperto() && !menuAiuto.getPannelloMenuAiutoAperto())
-            {
-                playerStop.Invoke();
-                menuAiuto.apriPannelloMenuAiuto();
-                PuntatoreMouse.abilitaCursore();
-                CambioCursore.cambioCursoreNormale();
-            }
-            else
-            {
-                negozio.animazioneNPCIdle();
-                uscitaRangeMenu.Invoke();
-                if (pannelloAperto)
-                {
-                    if(!PannelloMenu.pannelloIngredientiPiattoAperto && !PannelloMenu.pannelloConfermaPiattoAperto && !PannelloMenu.pannelloIngredientiGiustiSbagliatiAperto)
-                    {
-                        if (Input.GetKeyDown(KeyCode.Escape))
-                        {
-                            esciDaInterazioneCliente();
-                        }
-                        
-                    }
-                } 
-            }
-            if(negozio.getPannelloAperto() && !negozio.getPannelloConfermaAperto ())
-            {
-                if (Input.GetKeyDown(KeyCode.Escape))
-                {
-                    negozio.chiudiPannelloNegozio();
-                    PuntatoreMouse.disabilitaCursore();
-                    playerRiprendiMovimento.Invoke();
-                }
-            }
-            if (interazionePassanti.getPannelloInterazionePassantiAperto())
-            {
-                if (Input.GetKeyDown(KeyCode.Escape))
-                {
-                    interazionePassanti.chiudiPannelloInterazionePassanti();
-                    npcPassivo.stopAnimazioneParlata(gameObject.transform);
-                    PuntatoreMouse.disabilitaCursore();
-                    playerRiprendiMovimento.Invoke();
-                }
-            }
-            if (ricettarioScript.getRicettarioAperto())
-            {
-                if (Input.GetKeyDown(KeyCode.Escape))
-                {
-                    ricettarioScript.chiudiRicettario();
-                    PuntatoreMouse.disabilitaCursore();
-                    playerRiprendiMovimento.Invoke();
-                }
-            }
-            if (menuAiuto.getPannelloMenuAiutoAperto())
-            {
-                if (Input.GetKeyDown(KeyCode.Escape))
-                {
-                    menuAiuto.chiudiPannelloMenuAiuto();
-                    PuntatoreMouse.disabilitaCursore();
-                    playerRiprendiMovimento.Invoke();
-                }
-            }
-        }
-    }
-
-    private bool negozianteInquadrato()
+    private bool negoziantePuntato()
     {
         RaycastHit pcInquadrato;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out pcInquadrato, 2, layerUnityNPC))
@@ -281,26 +400,15 @@ public class Interactor : MonoBehaviour
         magazzino.chiudiPannelloMagazzino();
     }
 
-    private bool ricettarioPuntato()
+    private bool portaPuntata()
     {
-        RaycastHit ricettarioInquadrato;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out ricettarioInquadrato, 2, ricettario))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    private bool portaInquadrata()
-    {
-        RaycastHit portaInquadrata;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out portaInquadrata, 2, layerUnityTeleport))
+        RaycastHit portaPuntata;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out portaPuntata, 2, layerUnityTeleport))
         {
             // se l'oggetto visualizzato è interagibile
-            if (portaInquadrata.collider.GetComponent<InteractablePorta>() != false)
+            if (portaPuntata.collider.GetComponent<InteractablePorta>() != false)
             {
-                
-                destinazioneTeleport = portaInquadrata.collider.GetComponent<InteractablePorta>().posizioneTeleport;
+                destinazioneTeleport = portaPuntata.collider.GetComponent<InteractablePorta>().posizioneTeleport;
                 return true;
             }
         }
@@ -316,22 +424,6 @@ public class Interactor : MonoBehaviour
             if (pcInquadrato.collider.GetComponent<PannelloMagazzino>() != false)
             {
                 return true;
-            }
-        }
-        return false;
-    }
-
-    private bool NPCClientePuntato()
-    {
-        RaycastHit NPCpuntato;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out NPCpuntato, 2, layerUnityNPC))
-        {
-            // se l'oggetto visualizzato è interagibile
-            if (NPCpuntato.collider.GetComponent<Interactable>() != false)
-            {
-                IDClientePuntato = NPCpuntato.collider.GetComponent<Interactable>().IDCliente;
-                npc = NPCpuntato.collider.GetComponent<Interactable>();
-;               return true;
             }
         }
         return false;
@@ -361,29 +453,6 @@ public class Interactor : MonoBehaviour
     {
         mainCamera.transform.position = posizioneCamera.position;
     }
-
-    private void pannelloApertoChiuso()
-    {
-        pannelloAperto = !pannelloAperto;
-    }
-
-    private void interazioneCliente(int IDCliente)
-    {
-        muoviCameraPerInterazioneCliente();
-
-        playerStop.Invoke();
-        
-        pannelloMenuCliente.setCliente(IDClientePuntato, giocatore, npc);
-        
-        PuntatoreMouse.abilitaCursore();
-        pannelloApertoChiuso();
-    }
-
-    private void muoviCameraPerInterazioneCliente()
-    {
-        mainCamera.transform.position = posizioneCameraMenuCliente.transform.position;
-    }
-
 
     private void chiudiPannello()
     {
