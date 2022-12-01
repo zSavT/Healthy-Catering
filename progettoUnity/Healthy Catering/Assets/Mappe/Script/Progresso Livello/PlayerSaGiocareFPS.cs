1121692de7166a4026bbfe9bb2581d29f92b174c
zSavT <verioc3@gmail.com>
2022-12-01 09:47:51 +0100

    Migliorata flessibilità input Player Sa Giocare

--- progettoUnity/Healthy Catering/Assets/Mappe/Script/Progresso Livello/PlayerSaGiocareFPS.cs
+++ progettoUnity/Healthy Catering/Assets/Mappe/Script/Progresso Livello/PlayerSaGiocareFPS.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerSaGiocareFPS : MonoBehaviour
 {
 
     [SerializeField] GameObject pannello;
+    [SerializeField] GameObject bottoneNo;
+    private GameObject ultimoElementoSelezionato;
     // 0 = nessuno selezionato
     // 1 = si
     // -1 = no
@@ -20,6 +24,60 @@ public class PlayerSaGiocareFPS : MonoBehaviour
         pannello.SetActive(false);
         pannelloSaGiocareAperto = false;
         siOno = 0;
+    }
+
+    private void Update()
+    {
+        if (Utility.gamePadConnesso())
+            if (EventSystem.current.currentSelectedGameObject == null)
+                EventSystem.current.SetSelectedGameObject(bottoneNo);
+    }
+
+    void Awake()
+    {
+        InputSystem.onDeviceChange += OnDeviceChange;
+    }
+
+    void OnDestroy()
+    {
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    /// <summary>
+    /// Il metodo controlla e gestiscisce le periferiche di Input 
+    /// </summary>
+    /// <param name="device"></param>
+    /// <param name="change"></param>
+    public void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        switch (change)
+        {
+            case InputDeviceChange.Added:
+                // New Device.
+                break;
+            case InputDeviceChange.Disconnected:
+                ultimoElementoSelezionato = EventSystem.current.currentSelectedGameObject;
+                break;
+            case InputDeviceChange.Reconnected:
+                aggioraEventSystemPerControllerConnesso(ultimoElementoSelezionato);
+                break;
+            case InputDeviceChange.Removed:
+                // Remove from Input System entirely; by default, Devices stay in the system once discovered.
+                break;
+            default:
+                // See InputDeviceChange reference for other event types.
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Il metodo imposta come elemento selzionato dell'EventSystem l'oggetto passato in input
+    /// </summary>
+    /// <param name="elementoDaSelezionare">GameObject da impostare come elemento selezionato</param>
+    private void aggioraEventSystemPerControllerConnesso(GameObject elementoDaSelezionare)
+    {
+        if (Utility.gamePadConnesso())
+            EventSystem.current.SetSelectedGameObject(elementoDaSelezionare);
     }
 
     public void apriPannelloPlayerSaGiocareFPS()
